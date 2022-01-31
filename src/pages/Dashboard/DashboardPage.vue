e29c0df27e6e55af99c8db4290fe503125691f13
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-31 18:56:08 +0000

    #1 feat: application default pages

---
+++ src/pages/Dashboard/DashboardPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <section id="dashboard">
+        Dashboard
+    </section>
+</template>
+
+<script>
+export default {
+    name: "DashboardPage"
+}
+</script>
+
+<style scoped>
+
+</style>
