e7c369bc1ea673731057655d5e761da75d536246
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-07 18:25:56 +0000

    #4 feat: add drag and drop area to deploy page

--- src/pages/Deploy/DeployPage.vue
+++ src/pages/Deploy/DeployPage.vue
@@ -1,12 +1,20 @@
 <template>
-    <section id="deploy">
-        Deploy
+    <section id="deploy"
+             class="flex w-full h-full p-24"
+    >
+        <!-- App to be deployed in the cluster selection -->
+        <DragDrop/>
     </section>
 </template>
 
 <script>
+import DragDrop from "@/components/Deploy/DragDrop";
+
 export default {
-    name: "DeployPage"
+    name: "DeployPage",
+    components: {
+        DragDrop
+    }
 }
 </script>
 
