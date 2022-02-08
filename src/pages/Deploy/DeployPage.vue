0432bba56000ab43ee48e10bdfa89861b69768c9
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-08 12:08:41 +0000

    #5 feat: upload confirmation popup toggle & cancel/upload handling

--- src/pages/Deploy/DeployPage.vue
+++ src/pages/Deploy/DeployPage.vue
@@ -1,19 +1,51 @@
 <template>
     <section id="deploy"
-             class="flex w-full h-full p-24"
+             class="flex relative w-full h-full p-24"
     >
         <!-- App to be deployed in the cluster selection -->
-        <DragDrop/>
+        <DragDrop @uploadFile="toggle_popup"/>
+        <!-- Upload Confirmation PopUp -->
+        <UploadConfirmation v-if="file_selected" @buttonClick="confirm_upload"/>
     </section>
 </template>
 
 <script>
 import DragDrop from "@/components/Deploy/DragDrop";
+import UploadConfirmation from "@/components/Deploy/UploadConfirmation";
 
 export default {
     name: "DeployPage",
     components: {
+        UploadConfirmation,
         DragDrop
+    },
+    methods: {
+        /**
+         * Toggles upload confirmation popup
+         */
+        toggle_popup: function () {
+            this.file_selected = !this.file_selected;
+        },
+        /**
+         * Either cancels the upload or
+         * uploads the file to YakoMaster
+         * @param buttonID
+         * false: Cancel
+         * true: Upload
+         */
+        confirm_upload: function (buttonID) {
+            if ( buttonID ) {
+                // TODO: Upload file to YakoMaster
+            }
+
+            // Hides the popup
+            this.toggle_popup();
+        }
+    },
+    data() {
+        return {
+            file_selected: false,
+        }
     }
 }
 </script>
