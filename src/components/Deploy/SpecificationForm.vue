0d22957950ea2a3781c79bdc3a1b375e8fef28d7
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-30 14:13:41 +0100

    #19 feat: specification form layout

---
+++ src/components/Deploy/SpecificationForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex w-full h-full">
+        <div class="flex flex-col w-full h-full bg-white rounded-lg">
+            <h1 class="font-bold text-xl my-2"> App Requirements </h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SpecificationForm"
+}
+</script>
+
+<style scoped>
+
+</style>
