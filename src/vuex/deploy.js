4f20a2e642b6493b8a3703f979d4a90955df823d
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-02 11:21:27 +0100

    #19 feat: best nodes vuex getters & setters

--- src/vuex/deploy.js
+++ src/vuex/deploy.js
@@ -9,11 +9,17 @@ const deploy = {
         setName(state, name) {
             state.deploy_app_name = name;
         },
+        setBestNodes(state, nodes_list) {
+            state.best_nodes = nodes_list;
+        }
     },
     getters: {
         // gets the name of the app to be deployed in the cluster
         getDeployAppName(state) {
             return state.deploy_app_name;
+        },
+        getBestNodes(state) {
+            return state.best_nodes;
         }
     }
 }
