febb676490553009a6f67b5b05bf5b87c144afbe
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-02 12:19:13 +0100

    #22 feat: yakomasters list section

--- src/pages/Dashboard/DashboardPage.vue
+++ src/pages/Dashboard/DashboardPage.vue
@@ -2,7 +2,13 @@
     <section id="dashboard" class="flex flex-col w-full h-full p-5">
         <h1 class="font-bold text-2xl"> Dashboard </h1>
         <div class="flex w-full h-full">
+            <div class="flex w-1/2 h-full">
+                <!-- YakoMasters list -->
+                <div class="flex w-full h-1/2 bg-white rounded-xl p-5 space-y-2">
+                    <h2 class="font-bold font-xl"> Masters </h2>
 
+                </div>
+            </div>
         </div>
     </section>
 </template>
