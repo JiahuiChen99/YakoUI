71c089b2a8ec028be3f2ca6810f9d8c67c437731
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-02 12:04:20 +0100

    #21 feat: dashboard page layout

--- src/pages/Dashboard/DashboardPage.vue
+++ src/pages/Dashboard/DashboardPage.vue
@@ -1,6 +1,9 @@
 <template>
-    <section id="dashboard">
-        Dashboard
+    <section id="dashboard" class="flex flex-col w-full h-full p-5">
+        <h1 class="font-bold text-2xl"> Dashboard </h1>
+        <div class="flex w-full h-full">
+
+        </div>
     </section>
 </template>
 
