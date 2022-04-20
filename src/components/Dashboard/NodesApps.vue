4310f4644fe8395355cd2450e6e1c01f6f6b6036
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-20 18:35:12 +0100

    #26 refactor: nodesapps rename

--- src/components/Dashboard/NodesApps.vue
+++ src/components/Dashboard/NodesApps.vue
@@ -10,7 +10,7 @@
 <script>
 import TopYakoAgent from "@/components/Dashboard/TopYakoAgent";
 export default {
-    name: "TopAgentsSection",
+    name: "NodesApps",
     components: {
         TopYakoAgent
     },
