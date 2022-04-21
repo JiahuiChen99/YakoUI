319c2fd3139c52a8217e9c0971f77ddd2a03c902
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-21 15:10:12 +0100

    #26 feat: deploy apps component for dashboard apps list

---
+++ src/components/Dashboard/DeployApps.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex bg-primary_variant rounded-lg p-3 justify-between">
+        <!-- App name -->
+        <span> {{ app }} </span>
+        <div class="flex space-x-2">
+            <!-- Deploy app -->
+            <IButton class="hover:bg-secondary" :icon="deploy_icon"/>
+            <!-- Remove app -->
+            <IButton class="hover:bg-secondary" :icon="remove_icon"/>
+            <!-- Dots -->
+            <IButton class="hover:bg-secondary" :icon="dots_icon"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mdiDelete, mdiDotsVertical, mdiPlay} from "@mdi/js";
+import IButton from "@/components/UI/IButton";
+
+export default {
+    name: "DeployApps",
+    props: ['app'],
+    components: {
+        IButton,
+    },
+    data() {
+        return {
+            deploy_icon: mdiPlay,
+            remove_icon: mdiDelete,
+            dots_icon: mdiDotsVertical,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
