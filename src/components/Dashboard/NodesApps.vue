11fdade2dab19c34b0278612aa539697711ae999
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-21 15:10:50 +0100

    #26 feat: cluster apps list section in the dashboard

--- src/components/Dashboard/NodesApps.vue
+++ src/components/Dashboard/NodesApps.vue
@@ -4,14 +4,21 @@
         <div class="grid grid-cols-3 w-full gap-x-3">
             <TopYakoAgent v-for="(agent, index) in top_agents" :key="index" :agent="agent"/>
         </div>
+        <!-- Cluster apps list -->
+        <h2 class="font-bold text-xl"> Apps </h2>
+        <div class="overflow-y-scroll space-y-3">
+            <DeployApps v-for="(app, index) in apps_list" :key="index" :app="app"/>
+        </div>
     </div>
 </template>
 
 <script>
 import TopYakoAgent from "@/components/Dashboard/TopYakoAgent";
+import DeployApps from "@/components/Dashboard/DeployApps";
 export default {
     name: "NodesApps",
     components: {
+        DeployApps,
         TopYakoAgent
     },
     computed: {
