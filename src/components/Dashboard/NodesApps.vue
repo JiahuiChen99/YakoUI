5f12cd683341dd3521b560e7448a9cc78ed89368
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-22 17:12:47 +0100

    #21 feat: pulsating placeholders for top nodes section

--- src/components/Dashboard/NodesApps.vue
+++ src/components/Dashboard/NodesApps.vue
@@ -2,7 +2,14 @@
     <div class="flex flex-col w-1/2 h-full rounded-xl p-5 space-y-2">
         <h2 class="font-bold text-xl"> Top Nodes </h2>
         <div class="grid grid-cols-3 w-full gap-x-3">
-            <TopYakoAgent v-for="(agent, index) in top_agents" :key="index" :agent="agent"/>
+            <!-- Pulsating placeholders -->
+            <template v-if="top_agents.length === 0">
+                <div class="flex bg-white/50 w-auto h-auto aspect-square rounded-lg animate-pulse" v-for="index in 3" :key="index"/>
+            </template>
+            <!-- Deployed app top nodes -->
+            <template v-else>
+                <TopYakoAgent v-for="(agent, index) in top_agents" :key="index" :agent="agent"/>
+            </template>
         </div>
         <!-- Cluster apps list -->
         <h2 class="font-bold text-xl"> Apps </h2>
